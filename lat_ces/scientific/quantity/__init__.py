af592d3d961a342148377714bb66f621c694b372
susnjarfahrudin-ai <susnjarfahrudin@gmail.com>
2026-08-14 19:26:00 +0200

    ADAPT quantity package exports to recovered PhysicalQuantity

--- lat_ces/scientific/quantity/__init__.py
+++ lat_ces/scientific/quantity/__init__.py
@@ -1,12 +1,7 @@
 """LAT-CES Scientific Physical Quantity hardening layer (SCI 42-45)."""
 
-from .quantity import (
-    AuditRecord,
-    EvidenceLink,
-    MeasurementTrace,
-    PhysicalQuantity,
-    PhysicalQuantityRevisionManager,
-)
+from .audit import AuditRecord
+from .quantity import EvidenceLink, MeasurementTrace, PhysicalQuantity, PhysicalQuantityRevisionManager
 from .equation import Equation
 from .integrity import generate_equation_hash, generate_quantity_hash, verify_quantity_integrity
 
