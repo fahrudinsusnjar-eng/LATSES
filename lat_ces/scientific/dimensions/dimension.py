073cba54e2c1c7b5b8e9c95aaf65893fd5ff6e73
susnjarfahrudin-ai <susnjarfahrudin@gmail.com>
2026-08-17 05:04:50 +0200

    refactor(smc): point Scientific Dimensions at canonical Units core

--- lat_ces/scientific/dimensions/dimension.py
+++ lat_ces/scientific/dimensions/dimension.py
@@ -1,31 +1,12 @@
-from lat_ces.core.dimensions import (
-    ACCELERATION,
-    AMOUNT,
-    CURRENT,
-    DENSITY,
-    DIMENSIONLESS,
-    FORCE,
-    LENGTH,
-    LUMINOUS_INTENSITY,
-    MASS,
-    TEMPERATURE,
-    TIME,
-    VELOCITY,
-    Dimension,
+from __future__ import annotations
+
+from lat_ces.scientific.units.core import (
+    ACCELERATION, AMOUNT, CURRENT, DENSITY, DIMENSIONLESS, FORCE, LENGTH,
+    LUMINOUS_INTENSITY, MASS, TEMPERATURE, TIME, VELOCITY, Dimension,
 )
 
 __all__ = [
-    "Dimension",
-    "DIMENSIONLESS",
-    "LENGTH",
-    "MASS",
-    "TIME",
-    "CURRENT",
-    "TEMPERATURE",
-    "AMOUNT",
-    "LUMINOUS_INTENSITY",
-    "VELOCITY",
-    "DENSITY",
-    "ACCELERATION",
-    "FORCE",
+    "Dimension", "DIMENSIONLESS", "LENGTH", "MASS", "TIME", "CURRENT",
+    "TEMPERATURE", "AMOUNT", "LUMINOUS_INTENSITY", "VELOCITY", "DENSITY",
+    "ACCELERATION", "FORCE",
 ]
