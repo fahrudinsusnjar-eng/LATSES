1f59401d6574e4c32f18d7119b26681517476094
susnjarfahrudin-ai <susnjarfahrudin@gmail.com>
2026-08-19 14:04:15 +0200

    Revert "feat: reference house golden model and interactive engineering showroom"

--- lat_ces/materials/__init__.py
+++ lat_ces/materials/__init__.py
@@ -1,12 +1,5 @@
-"""Read-only engineering access to material information and catalogs."""
+"""Read-only engineering access to manufacturer-declared material data."""
 
-from .building_catalog import BuildingCatalogItem, BuildingMaterialCatalog, GlazingOption
 from .catalog_reader import MaterialCatalog, MaterialRecord
 
-__all__ = [
-    "BuildingCatalogItem",
-    "BuildingMaterialCatalog",
-    "GlazingOption",
-    "MaterialCatalog",
-    "MaterialRecord",
-]
+__all__ = ["MaterialCatalog", "MaterialRecord"]
